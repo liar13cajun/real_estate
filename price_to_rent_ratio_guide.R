a77fa698d10f2e5c1366fec979d89fecc2325791
liar13cajun <91923298+liar13cajun@users.noreply.github.com>
2025-01-08 21:15:14 +1030

    update

---
+++ price_to_rent_ratio_guide.R
@@ -0,0 +1,13 @@
+# price to rent ratio 
+# price to annual rent
+
+
+# get rent average data for house
+
+# get rent avergae data for flat
+
+# each post code
+
+# work with each post code
+ 
+# get housing price history across suburb 
